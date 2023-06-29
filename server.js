612953fe7b7bd8514787028b8e364aa5d30e1461
axemonkey <hi+github@clivemurray.com>
2023-06-29 10:54:11 +0100

    delete old JS app files

--- server.js
+++
@@ -1,9 +0,0 @@
-// import {cubistApp} from './cubi.st/app.js';
-
-// const env = process.env.NODE_ENV || 'development';
-
-// if (env === 'development') {
-// 	cubistApp.listen(7777);
-// } else if (env === 'production') {
-// 	cubistApp.listen(process.env.PORT);
-// }
