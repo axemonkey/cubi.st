aa3a4287231cdcc4e73445a80205b6f74897f02b
axemonkey <hi+github@clivemurray.com>
2025-09-14 15:59:22 +0100

    switch to SN fe tooling

--- _site/public/js/modules/toasties.js
+++
@@ -1,9 +0,0 @@
-const toasty = options => {
-	const {text, type = 'info'} = options;
-	Toastify({/* eslint-disable-line new-cap */
-		text,
-		className: `toast-${type}`,
-	}).showToast();
-};
-
-export {toasty};
